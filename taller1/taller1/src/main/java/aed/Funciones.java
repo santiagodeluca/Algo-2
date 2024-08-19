2134654f1e5f8ad489841247895ae04986effe00
santiagodeluca <santidelu510@gmail.com>
2024-08-19 19:26:14 -0300

    taller 1 correccion

--- taller1/taller1/src/main/java/aed/Funciones.java
+++ taller1/taller1/src/main/java/aed/Funciones.java
@@ -2,27 +2,22 @@ package aed;
 
 class Funciones {
     int cuadrado(int x) {
-        // COMPLETAR
         return x * x;
     }
 
     double distancia(double x, double y) {
-        // COMPLETAR
         return Math.sqrt(x*x + y*y);
     }
 
     boolean esPar(int n) {
-        // COMPLETAR
         return n % 2 == 0;
     }
 
     boolean esBisiesto(int n) {
-        // COMPLETAR
         return ((n % 4 == 0) && (n % 100 != 0)) || n % 400 == 0;
     }
 
     int factorialIterativo(int n) {
-        // COMPLETAR
         int res = 1;
         for (int i = n; i > 1; i--) {
             res *= i;
@@ -31,7 +26,6 @@ class Funciones {
     }
 
     int factorialRecursivo(int n) {
-        // COMPLETAR
         if (n == 1 || n == 0) {
             return 1;
         }
@@ -41,7 +35,6 @@ class Funciones {
     }
 
     boolean esPrimo(int n) {
-        // COMPLETAR
         if (n == 1 || n == 0) {
             return false;
         }
@@ -54,7 +47,6 @@ class Funciones {
     }
 
     int sumatoria(int[] numeros) {
-        // COMPLETAR
         int res = 0;
         for (int x : numeros){
             res += x;
@@ -63,18 +55,16 @@ class Funciones {
     }
 
     int busqueda(int[] numeros, int buscado) {
-        // COMPLETAR
         for (int i = numeros.length - 1; i >= 0; i--){
             if (numeros[i] == buscado){
                 return i;
             }
         }
-        //?
+        //La especificación no aclara qué hacer en el caso que incumple el requiere (lo cual está bien), pero como java exige algún return elijo -1.
         return -1;
     }
 
     boolean tienePrimo(int[] numeros) {
-        // COMPLETAR
         for (int x : numeros){
             if (esPrimo(x)){
                 return true;
@@ -84,7 +74,6 @@ class Funciones {
     }
 
     boolean todosPares(int[] numeros) {
-        // COMPLETAR
         for (int x : numeros){
             if (!esPar(x)){
                 return false;
@@ -94,7 +83,6 @@ class Funciones {
     }
 
     boolean esPrefijo(String s1, String s2) {
-        // COMPLETAR
         for (int i = s1.length() - 1; i >= 0; i--){
             if (s1.length() > s2.length() || s1.charAt(i) != s2.charAt(i)){
                 return false;
@@ -104,7 +92,6 @@ class Funciones {
     }
     
     boolean esSufijo(String s1, String s2) {
-        // COMPLETAR
         for (int i = s1.length() - 1; i >= 0; i--){
             if (s1.length() > s2.length() || s1.charAt(i) != s2.charAt((s2.length() - s1.length()) + i)){
                 return false;
